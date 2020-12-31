3e8ed1a712646a3b6a867912f11f1b4d5d06bc69
Mafciejewicz <melonik0.9846@gmail.com>
2020-12-31 01:27:16 +0100

    Add assets loading

---
+++ src/typings/assets.d.ts
@@ -0,0 +1,4 @@
+declare module "*.ps1" {
+    const assetPath: string;
+    export default assetPath;
+}
--- tsdx.config.js
+++ tsdx.config.js
@@ -1,14 +1,22 @@
-const shebang = require('rollup-plugin-preserve-shebang');
+const shebang = require('rollup-plugin-preserve-shebang')
+const url = require('@rollup/plugin-url')
 const path = require('path')
 
 module.exports = {
     rollup: config => {
-        config.plugins.unshift(shebang(
-            {
+        config.plugins.unshift(
+            url({
+                include: ['**/*.ps1'],
+                limit: 0
+            })
+        )
+        config.plugins.unshift(
+            shebang({
                 entry: path.resolve(process.cwd(), 'src/index.ts')
-            }
-        ));
+            })
+        );
 
-        return config;
+
+        return config
     }
 }
